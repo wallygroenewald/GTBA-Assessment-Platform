e15ddd6a622ed833aa45804575a9602be66974fd
Walter Groenewald <waltergroenewald@Walters-MacBook-Pro.local>
2026-06-06 06:19:57 +0200

    Added Dockerfile for Cloud Run

--- app.py
+++ app.py
@@ -801,6 +801,8 @@ def logout():
 # RUN APP
 # =========================================
 
-if __name__ == '__main__':
+import os
 
-    app.run(host='0.0.0.0', port=5001)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host='0.0.0.0', port=port)
